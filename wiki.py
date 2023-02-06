e8f670b213edd90403b72f4dfdc44519beb13d0d
Tristan Perard <tperard@aair13-6mg944.windows.csc.fi>
2023-02-06 16:13:43 +0200

    add scraping images, update to f-strings

--- wiki.py
+++ wiki.py
@@ -1,5 +1,6 @@
 import wikipediaapi
 import sys
+import requests
 
 from bs4 import BeautifulSoup
 
@@ -8,10 +9,20 @@ wiki_wiki = wikipediaapi.Wikipedia('fi')
 if len(sys.argv) != 2:
     sys.exit("Missing bird's name as argument")
 else:
+    # Get info for our bird
     bird = sys.argv[1]
     print("Searching: %s" % bird)
     page_py = wiki_wiki.page(bird)
 
-    print("Page - Exists: %s" % page_py.exists())
-    print("Page - Title: %s" % page_py.title)
-    print("Page - URL: %s" % page_py.fullurl)
+    # Let's make the soup!
+    page = requests.get(page_py.fullurl).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    for raw_img in soup.find_all('img'):
+        image = raw_img.get('src')
+        break
+
+    print(f"Page - Exists: {page_py.exists()}")
+    print(f"Page - Title: {page_py.title}")
+    print(f"Page - URL: {page_py.fullurl}")
+    print(f"Page - Image URL: {image}")
