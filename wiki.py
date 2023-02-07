17d72105b825816a8c8510381f802de105ff41d4
Tristan Perard <tperard@aair13-6mg944.windows.csc.fi>
2023-02-07 09:11:29 +0200

    add regex search

--- wiki.py
+++ wiki.py
@@ -1,6 +1,7 @@
 import wikipediaapi
 import sys
 import requests
+import regex
 
 from bs4 import BeautifulSoup
 
@@ -15,14 +16,20 @@ else:
     page_py = wiki_wiki.page(bird)
 
     # Let's make the soup!
+    # Query website and return html page
     page = requests.get(page_py.fullurl).text
+    # Parse html using BeautifulSoup
     soup = BeautifulSoup(page, 'html.parser')
 
+    # Loop to find image from the infobox
+    # Stop it when found the img files. Regex to exclude svg files
     for raw_img in soup.find_all('img'):
-        image = raw_img.get('src')
-        break
+        image_lnk = raw_img.get('src')
+        if regex.search('wikipedia/.*/thumb/', image_lnk) and not regex.search('.svg', image_lnk):
+            image_url = image_lnk
+            break
 
     print(f"Page - Exists: {page_py.exists()}")
     print(f"Page - Title: {page_py.title}")
     print(f"Page - URL: {page_py.fullurl}")
-    print(f"Page - Image URL: {image}")
+    print(f"Page - Image URL: {image_url}")
