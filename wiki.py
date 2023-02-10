5a7df585eb114ae360ac7fc16315492a72b6e321
Tristan Perard <tperard@aair13-6mg944.windows.csc.fi>
2023-02-10 13:54:09 +0200

    WIP, load and dump into json

--- wiki.py
+++ wiki.py
@@ -2,22 +2,29 @@ import wikipediaapi
 import sys
 import requests
 import regex
+import json
+import os
 
 from bs4 import BeautifulSoup
 
 wiki_wiki = wikipediaapi.Wikipedia('fi')
+LIST_IN_PATH = os.path.join(os.path.dirname(__file__), "bird_species_id_testing.json")
+LIST_OUT_PATH = os.path.join(os.path.dirname(__file__), "bird_species_data.json")
 
-try:
-    # Get info for our bird
-    if len(sys.argv) != 2:
-        bird = input("Missing bird's name as argument, please enter the name of the bird: ")
-    else:
-        bird = sys.argv[1]
+if os.path.exists(LIST_IN_PATH):
+    with open(LIST_IN_PATH, "r") as f_in:
+        LIST = json.load(f_in)
+else:
+    sys.exit("Where's the list?")
 
-    print(f"Searching: {bird}")
-    page_py = wiki_wiki.page(bird)
+# Get info from our json file
+for birds in LIST:
+    print(f"Searching: {birds}")
+    page_py = wiki_wiki.page(birds)
+    title = page_py.title
+    url = page_py.fullurl
 
-    # Let's make the soup!
+    # Let's make the soup
     # Query website and return html page
     page = requests.get(page_py.fullurl).text
     # Parse html using BeautifulSoup
@@ -27,20 +34,15 @@ try:
     # Stop it when found the img files. Regex to exclude svg files
     for raw_img in soup.find_all('img'):
         image_lnk = raw_img.get('src')
-        if regex.search('wikipedia/.*/thumb/', image_lnk) and not regex.search('.svg', image_lnk):
-            image_url = image_lnk
+        if regex.search('wikipedia/.*/thumb', image_lnk) and not regex.search('.svg', image_lnk):
+            image_url = "https:" + image_lnk
             break
         else:
             image_url = "Not found"
 
-    print(f"Page - Exists: {page_py.exists()}")
-    print(f"Page - Title: {page_py.title}")
-    print(f"Page - URL: {page_py.fullurl}")
-    if image_url == image_lnk:
-        print(f"Page - Image URL: https:{image_url}")
-    else:
-        print(f"Page - Image URL: {image_url}")
+    data = {"Title": title, "URL": url, "Image URL": image_url}
 
-except KeyboardInterrupt:
-    print("Hard exit, bye!")
-    raise
+    with open(LIST_OUT_PATH, "a") as f_out:
+        json.dump(data, f_out)
+
+sys.exit("Process completed")
