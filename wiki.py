a081226ac661c4dfad6202875c614fbdf2d61e69
Tristan Perard <tperard@aair13-6mg944.windows.csc.fi>
2023-02-13 14:51:20 +0200

    WIP, functions read from input and file

--- wiki.py
+++ wiki.py
@@ -7,27 +7,13 @@ import json
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-LIST_IN_PATH = BASE_DIR / "bird_species_id.json"
-LIST_OUT_PATH = BASE_DIR / "bird_species_array.json"
-
 wiki_wiki = wikipediaapi.Wikipedia('fi')
-birdsArray = []
 
-if LIST_IN_PATH.exists():
-    with open(LIST_IN_PATH, "r") as f_in:
-        LIST = json.load(f_in)
-else:
-    sys.exit("Error, birds' list missing")
+def read_from_input(birdName):
+    print(f"Searching: {birdName}")
+    page_py = wiki_wiki.page(birdName)
 
-# Get info from our json file
-for birds in LIST:
-    print(f"Searching: {birds}")
-    page_py = wiki_wiki.page(birds)
-    title = page_py.title
-    url = page_py.fullurl
-
-    # Let's make the soup
+    # Let's make the soup!
     # Query website and return html page
     page = requests.get(page_py.fullurl).text
     # Parse html using BeautifulSoup
@@ -37,19 +23,78 @@ for birds in LIST:
     # Stop it when found the img files. Regex to exclude svg files
     for raw_img in soup.find_all('img'):
         image_lnk = raw_img.get('src')
-        if regex.search('wikipedia/.*/thumb', image_lnk) and not regex.search('.svg', image_lnk):
-            image_url = "https:" + image_lnk
+        if regex.search('wikipedia/.*/thumb/', image_lnk) and not regex.search('.svg', image_lnk):
+            image_url = image_lnk
             break
         else:
             image_url = "Not found"
 
-    # Gathering data and append it to birdsArray
-    data = {birds:{"URL": url, "Image URL": image_url}}
-    birdsArray.append(data)
+    print(f"Page - Exists: {page_py.exists()}")
+    print(f"Page - Title: {page_py.title}")
+    print(f"Page - URL: {page_py.fullurl}")
+    if image_url == image_lnk:
+        print(f"Page - Image URL: https:{image_url}")
+    else:
+        print(f"Page - Image URL: {image_url}")
 
-# Dump data to a new .json file
-with open(LIST_OUT_PATH, "w") as f_out:
-        json.dump(birdsArray, f_out, indent=4)
+def read_from_file (birdFilename):
+    BASE_DIR = Path(__file__).resolve().parent
+    LIST_OUT_PATH = BASE_DIR / "bird_species_array.json"
+    
+    birdsArray = []
+    birdFile = BASE_DIR / birdFilename
 
-# End of process
-sys.exit("Process completed")
+    if birdFile.exists():
+        with open(birdFile, "r") as f_in:
+            LIST = json.load(f_in)
+    else:
+        sys.exit(f"Error, file {birdFilename} doesn't exist")
+
+    # Get info from our json file
+    for birds in LIST:
+        print(f"Searching: {birds}")
+        page_py = wiki_wiki.page(birds)
+        title = page_py.title
+        url = page_py.fullurl
+
+        # Let's make the soup
+        # Query website and return html page
+        page = requests.get(page_py.fullurl).text
+        # Parse html using BeautifulSoup
+        soup = BeautifulSoup(page, 'html.parser')
+
+        # Loop to find image from the infobox
+        # Stop it when found the img files. Regex to exclude svg files
+        for raw_img in soup.find_all('img'):
+            image_lnk = raw_img.get('src')
+            if regex.search('wikipedia/.*/thumb', image_lnk) and not regex.search('.svg', image_lnk):
+                image_url = "https:" + image_lnk
+                break
+            else:
+                image_url = "Not found"
+
+        # Gathering data and append it to birdsArray
+        data = {birds:{"URL": url, "Image URL": image_url}}
+        birdsArray.append(data)
+
+    # Dump data to a new .json file
+    with open(LIST_OUT_PATH, "w") as f_out:
+            json.dump(birdsArray, f_out, indent=4)
+
+    # End of process
+    sys.exit("Process completed")
+
+if __name__ == "__main__":
+    # Get info for our bird
+    try:
+        if len(sys.argv) != 2:
+            name = input("Missing bird's name as argument, please enter the name of the bird: ")
+        elif sys.argv != 
+        else:
+            name = sys.argv[1]
+
+    except KeyboardInterrupt:
+        print("Hard exit, bye!")
+        raise
+
+    read_from_input(name)
