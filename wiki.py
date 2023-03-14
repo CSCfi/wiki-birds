54954bc1a6a1974b70377689c5573290f245c4ec
Tristan <74349933+Socuz@users.noreply.github.com>
2023-03-14 12:42:11 +0200

    pylint recommendations

--- wiki.py
+++ wiki.py
@@ -1,8 +1,19 @@
-import wikipediaapi
+"""
+Script that help you to get information from a bird's file.
+It will reach Wikipedia (FI) to retrieve:
+- If Page exists
+- Title
+- URL
+- Image URL
+
+The output will be exported into a file named bird_species_array.json
+"""
 import sys
+import json
+import wikipediaapi
 import requests
 import regex
-import json
+
 
 from bs4 import BeautifulSoup
 from pathlib import Path
@@ -15,7 +26,7 @@ wiki_wiki = wikipediaapi.Wikipedia('fi')
 birdsArray = []
 
 if LIST_IN_PATH.exists():
-    with open(LIST_IN_PATH, "r") as f_in:
+    with open(LIST_IN_PATH, "r", encoding="utf-8") as f_in:
         LIST = json.load(f_in)
 else:
     sys.exit("Error, birds' list missing")
@@ -29,7 +40,7 @@ for birds in LIST:
 
     # Let's make the soup
     # Query website and return html page
-    page = requests.get(page_py.fullurl).text
+    page = requests.get(page_py.fullurl, timeout= 10).text
     # Parse html using BeautifulSoup
     soup = BeautifulSoup(page, 'html.parser')
 
@@ -38,18 +49,17 @@ for birds in LIST:
     for raw_img in soup.find_all('img'):
         image_lnk = raw_img.get('src')
         if regex.search('wikipedia/.*/thumb', image_lnk) and not regex.search('.svg', image_lnk):
-            image_url = "https:" + image_lnk
+            IMAGE_URL = "https:" + image_lnk
             break
-        else:
-            image_url = "Not found"
+        IMAGE_URL = "Not found"
 
     # Gathering data and append it to birdsArray
-    data = {birds:{"URL": url, "Image URL": image_url}}
+    data = {birds:{"URL": url, "Image URL": IMAGE_URL}}
     birdsArray.append(data)
 
 # Dump data to a new .json file
-with open(LIST_OUT_PATH, "w") as f_out:
-        json.dump(birdsArray, f_out, indent=4)
+with open(LIST_OUT_PATH, "w", encoding="utf-8") as f_out:
+    json.dump(birdsArray, f_out, indent=4)
 
 # End of process
 sys.exit("Process completed")
